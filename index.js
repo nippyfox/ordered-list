82f8e5c1a84d4c3e7f51f57fac409e94352903c0
iuriinesterov <55027018+iuriinesterov@users.noreply.github.com>
2020-12-30 19:15:04 +0300

    Start working

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const url = require('url')
+const {app, BrowserWindow} = require('electron')
+
+let win
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 500,
+        height: 200,
+        resizable: false,
+        icon: __dirname + "/img/icon.png"
+    })
+
+    win.setMenu(null)
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    app.quit()
+})
